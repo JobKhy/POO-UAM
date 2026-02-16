8a8b4f45c6f121bba244ea670aa705042bac092c
JobKhy <pina.martinez.gael.job@gmail.com>
2026-02-16 16:22:22 -0600

    Complete and improve tarea_2

--- tarea_2.cpp
+++ tarea_2.cpp
@@ -1,102 +1,147 @@
 #include <iostream>
 
-class conjunto {
-	int values[100];
-	int s;
-	
-	
-	public:
-		conjunto() {
-      		s = 0;
-			for(int i = 0; i < 100; i++){
-				this->values[i] = 0;
-			}	
-   		}	
-   		
-   		conjunto(const conjunto& v) {
-      		this->s = v.s;
-      		for (int i = 0; i < v.s; ++i) {
-         		this->values[i] = v.values[i];
-      		}		
-   		}
-   		
-   		conjunto(std::initializer_list<int> c){
-		  	this->s = 0;
-		    for (int v : c) {
-		    	inserta(v);
-		    }
+class conjunto
+{
+ int values[100];
+ int s;
+
+public:
+ conjunto()
+ {
+		s = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			this->values[i] = 0;
+		}
+ }
+
+ conjunto(std::initializer_list<int> c)
+ {
+		this->s = 0;
+		for (int i = 0; i < 100; i++)
+			this->values[i] = 0;
+
+		for (int v : c)
+		{
+			inserta(v);
+		}
+ }
+
+ conjunto(const conjunto &oc)
+ {
+		this->s = oc.s;
+		for (int i = 0; i < oc.s; i++)
+		{
+			this->values[i] = oc.values[i];
+		}
+ }
+
+ conjunto &operator=(const conjunto &oc)
+ {
+		if (this == &oc)
+			return *this;
+
+		this->s = oc.s;
+		for (int i = 0; i < oc.s; ++i)
+		{
+			this->values[i] = oc.values[i];
+		}
+		return *this;
+ }
+
+ bool inserta(int value)
+ {
+		if (value > 100 || value < 0 || s >= 100)
+			return false;
+
+		for (int i = 0; i < s; i++)
+		{
+			if (values[i] == value)
+				return false;
 		}
 
-   		void operator=(const conjunto& v) {
-      		this->s = v.s;
-      		for (int i = 0; i < v.s; ++i) {
-         		this->values[i] = v.values[i];
-      		}
-   		}
-		
-		bool inserta(int value){
-			if( this->values[value-1] == value && value != 0){
-				return false;
+		int i = s - 1;
+		while (i >= 0 && values[i] > value)
+		{
+			values[i + 1] = values[i];
+			i--;
+		}
+
+		this->values[i + 1] = value;
+		this->s++;
+		return true;
+ }
+
+ bool elimina(int value)
+ {
+		int idx = -1;
+		for (int i = 0; i < s; i++)
+		{
+			if (this->values[i] == value)
+			{
+				idx = i;
+				break;
 			}
-			this->values[value-1] == value;
-			this->s++;
-			return true;
 		}
-		
-		bool elimina (int value){
-			if( this->values[value-1] == 0 && value != 0){
-				return false;
-			}
-			this->values[value-1] == 0;
-			this->s--;
-			return true;
+
+		if (idx == -1)
+			return false;
+
+		for (int i = idx; i < s - 1; i++)
+		{
+			this->values[i] = this->values[i + 1];
 		}
-		
-		int size(){
-			return this->s;
-		}
-		
-		int operator[](int i){
-			return this->values[i];
-		}
-		
+
+		s--;
+		return true;
+ }
+
+ int size() const
+ {
+		return this->s;
+ }
+
+ int operator[](int i) const
+ {
+		return this->values[i];
+ }
 };
 
-
-bool operator==(const conjunto a, const conjunto b){
-	if (a.size() != b.size()) return false;
-	
-	for(int i = 0; i<a.size(); i++){
-		if(a[i] != b[i]) return false;
-	}
-	return true;
+bool operator==(const conjunto &a, const conjunto &b)
+{
+ if (a.size() != b.size())
+		return false;
+ for (int i = 0; i < a.size(); i++)
+ {
+		if (a[i] != b[i])
+			return false;
+ }
+ return true;
 }
 
-bool operator!=(const conjunto a, const conjunto b){
-	if (a.size() != b.size()) return true;
-	
-	for(int i = 0; i<a.size(); i++){
-		if(a[i] != b[i]) return true;
-	}
-	return false;
+bool operator!=(const conjunto a, const conjunto b)
+{
+ return !(a == b);
 }
 
-int main (){
-	conjunto c;
-	bool b1 = c.inserta(3);
-	bool b2 = c.inserta(1);
-	bool b3 = c.inserta(3);
-	
-	std::cout << b1 << " "
-			  << b2 << " "
-			  << b3 << "\n";
-			  
-	std::cout << c.size( ) << "\n";
-	
-	for (int i = 0; i < c.size( ); ++i) {
+int main()
+{
+ conjunto c;
+ bool b1 = c.inserta(3);
+ bool b2 = c.inserta(1);
+ bool b3 = c.inserta(3);
+
+ std::cout << b1 << " "
+											<< b2 << " "
+											<< b3 << "\n";
+
+ std::cout << c.size() << "\n";
+
+ for (int i = 0; i < c.size(); ++i)
+ {
 		std::cout << c[i] << " ";
-	}
-	std::cout << "\n";
-	conjunto d = c;
-	std::cout << (c == d) << "\n";
+ }
+ std::cout << "\n";
+ conjunto d = c;
+ std::cout << (c == d) << "\n";
 }
